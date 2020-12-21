0b406899b873acb357fcbee6a756d4c87421cbf1
Philipp Kollmar <ph-kollmar@web.de>
2020-12-21 14:20:42 +0100

    added pickColor function and tests

---
+++ src/colors.js
@@ -0,0 +1,12 @@
+module.exports = {
+
+    RED: 'RED',
+    GREEN: 'GREEN',
+    YELLOW: 'YELLOW',
+    BLUE: 'BLUE',
+    PURPLE: 'PURPLE',
+    ORANGE: 'ORANGE',
+    PINK: 'PINK',
+    BROWN: 'BROWN'
+
+}
---
+++ src/mastermind.test.js
@@ -0,0 +1,29 @@
+const colors = require('./colors');
+const { pickColor } = require('./mastermind')
+
+describe('pickColor', () => {
+    it('should take a randomfn and return a color based on that', () => {
+        expect(pickColor(() => 0.1)).toEqual(colors.RED);
+    });
+
+    [
+        { valueRange: 0.125, color: colors.RED },
+        { valueRange: 0.25, color: colors.GREEN },
+        { valueRange: 0.375, color: colors.YELLOW },
+        { valueRange: 0.5, color: colors.BLUE },
+        { valueRange: 0.625, color: colors.PURPLE },
+        { valueRange: 0.75, color: colors.ORANGE },
+        { valueRange: 0.875, color: colors.PINK },
+        { valueRange: 1, color: colors.BROWN }
+    ].forEach(({ valueRange, color }) => {
+        it(`should return ${color} for value in Range of ${valueRange}`, () => {
+            expect(pickColor(() => { return valueRange - 0.001 })).toEqual(color)
+        })
+    })
+
+    it('it should throw on function that return more than 1.0', () => {
+        expect(() => {
+            pickColor(() => 1.0)
+        }).toThrow()
+    })
+})
